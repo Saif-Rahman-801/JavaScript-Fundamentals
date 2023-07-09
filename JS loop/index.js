6c839b21dd23c3d529ba1b38f4f439413847410a
Saif-Rahman-801 <saifurs102@gmail.com>
2023-07-09 18:56:53 +0600

    while loop

--- JS loop/index.js
+++ JS loop/index.js
@@ -11,5 +11,26 @@ for (let i = 9; i > 0; i--) {
 let myArray = ["one", "two", "three", "four", "five", "six"];
 let length = myArray.length;
 for (let i = length - 1; i >= 0; i--) {
-  console.log(myArray[i]);
+  // console.log(myArray[i]);
+}
+
+// While loop
+
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+let i2 = 10;
+while (i2 >= 0) {
+  console.log(i);
+  i2--;
+}
+
+// let myArray = ["one", "two", "three", "four", "five", "six"];
+let i3 = 0;
+while (i3 < myArray.length) {
+  console.log(myArray[i3]);
+  i3++;
 }
