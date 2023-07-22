90574d4f4bb20e1b1e587dc8d4b7d07baa4a694c
Saif-Rahman-801 <saifurs102@gmail.com>
2023-07-22 22:41:13 +0600

    object vs object

--- Object/object.js
+++ Object/object.js
@@ -7,8 +7,51 @@ const singlePerson = {
     console.log(`Hello ${this.name}`);
   },
 };
+// console.log(singlePerson["name"]);
+// console.log(singlePerson.name);
+// singlePerson.sayHello();
+// console.log(singlePerson[person]);
+// console.log(singlePerson[personTwo]());
 
-console.log(singlePerson.name);
-singlePerson.sayHello();
-console.log(singlePerson[person]);
-console.log(singlePerson[personTwo]());
+let obj = {
+  name: "Saifur Rahman Sakib",
+  skills: {
+    frontend: ["HTML", "CSS", "JS", "REACT"],
+    backend: ["NODE JS", "EXPRESS JS", "MONGO DB"],
+  },
+  address: {
+    city: "Chittagong",
+    country: "Bangladesh",
+  },
+  calc: (a, b) => {
+    return a + b;
+  },
+};
+
+let skill = obj.skills.backend[0];
+console.log(skill);
+console.log(obj.calc(2, 3));
+console.log(obj.skills["backend"]);
+console.log(obj["skills"].frontend);
+
+obj.interest = ["JavaScript", "React", "Node JS", "Mongo DB"];
+// console.log(obj);
+console.log(obj.interest);
+
+const objTwo = {
+  name: "Saifur Rahman Sakib",
+  skills: {
+    frontend: ["HTML", "CSS", "JS", "REACT"],
+    backend: ["NODE JS", "EXPRESS JS", "MONGO DB"],
+  },
+  address: {
+    city: "Chittagong",
+    country: "Bangladesh",
+  },
+  calc: (a, b) => {
+    return a + b;
+  },
+};
+
+console.log(obj == objTwo);
+console.log({} == {});
