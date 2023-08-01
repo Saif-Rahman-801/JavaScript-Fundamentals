1f5c577e015347e3e13f61b4fff6dec00ee83d4e
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-01 12:40:33 +0600

    callback with asynchronous behaviour

--- Asynchronous JS/callback.js
+++ Asynchronous JS/callback.js
@@ -1,5 +1,5 @@
 const displayer = (some) => {
-  console.log(some);
+  // console.log(some);
 };
 
 const calculator = (a, b, callback) => {
@@ -9,12 +9,45 @@ const calculator = (a, b, callback) => {
 
 calculator(5, 5, displayer);
 
-console.log(1);
-setTimeout(function () {
-  console.log(2);
-}, 2000);
+// console.log(1);
+// setTimeout(function () {
+//   console.log(2);
+// }, 2000);
 
-setInterval(() => {
-  console.log(2.1);
-}, 2000);
-console.log(3);
+// setInterval(() => {
+//   console.log(2.1);
+// }, 2000);
+// console.log(3);
+
+const payment = true;
+const enroll = (callback) => {
+  console.log("Enrollment processing");
+  setTimeout(() => {
+    if (payment === true) {
+      callback();
+    } else {
+      console.log("Payment failed");
+    }
+  }, 2000);
+};
+
+const mark = 70;
+const progress = (callback) => {
+  console.log("Course in progress");
+  setTimeout(() => {
+    if (mark >= 80) {
+      callback();
+    } else {
+      console.log("you don't have sufficient number");
+    }
+  }, 3000);
+};
+
+const getCertificate = () => {
+  console.log("Certificate processing!");
+  setTimeout(() => {
+    console.log("Congrats! you are certified");
+  }, 4000);
+};
+
+enroll(() => progress(getCertificate));
