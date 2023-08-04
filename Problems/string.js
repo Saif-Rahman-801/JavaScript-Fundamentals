a19c371874d353e478e880a06cfbfa04f30a1427
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-04 19:45:33 +0600

    string methods

---
+++ Problems/string.js
@@ -0,0 +1,10 @@
+const namer = "Saif Rahman";
+const dummy = "Nothing";
+console.log(namer.length);
+
+if (namer.toLowerCase() === dummy.toLowerCase()) {
+  console.log("true");
+}
+
+const doesExist = namer.includes("Rahman");
+console.log(doesExist);
