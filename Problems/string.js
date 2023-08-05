342a46d1c731e8628d60760d89b5a82abb3f5353
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-05 18:43:51 +0600

    more practice problems

--- Problems/string.js
+++ Problems/string.js
@@ -1,10 +1,42 @@
 const namer = "Saif Rahman";
 const dummy = "Nothing";
-console.log(namer.length);
+// console.log(namer.length);
 
 if (namer.toLowerCase() === dummy.toLowerCase()) {
   console.log("true");
 }
 
 const doesExist = namer.includes("Rahman");
-console.log(doesExist);
+// console.log(doesExist);
+let song = "Tumi bondhu Kala Pakhi, ami jeno ki?";
+const splited = song.split(" ");
+// const splited = song.split("");
+// const splited = song.split(",");
+
+// console.log(song, splited);
+
+const sliced = song.slice(1, 7);
+// console.log(sliced);
+
+/* let num = 30;
+let num1 = 20;
+
+const gap = Math.abs(num1 - num);
+console.log(gap);
+
+if (gap <= 5) {
+  console.log("You guys can date");
+} else {
+  console.log("you guys stay apart");
+} */
+
+const number = 3.1416;
+const roundedU = Math.ceil(number);
+const roundedD = Math.floor(number);
+// console.log(roundedU);
+// console.log(roundedD);
+
+for (let i = 0; i < 10; i++) {
+  let random = Math.round(Math.random() * 6);
+  console.log(random);
+}
