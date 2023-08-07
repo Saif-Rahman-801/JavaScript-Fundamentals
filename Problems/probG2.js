b004c660f8e7db0de4251ef7b9786fff91945d54
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-07 16:07:12 +0600

    gems-dimond

--- Problems/probG2.js
+++ Problems/probG2.js
@@ -17,4 +17,29 @@ function findingBadData(arr) {
 }
 
 let arr = [1, 2, 3, -3, -2, -1, -9];
-console.log(findingBadData(arr));
+// console.log(findingBadData(arr));
+
+function gemsToDiamond(x, y, z) {
+  let gemsOneValue = 21;
+  let gemsTwoValue = 32;
+  let gemsThreeValue = 43;
+  let friendOnetotalGem = gemsOneValue * x;
+  let friendTwototalGem = gemsTwoValue * y;
+  let friendThreetotalGem = gemsThreeValue * z;
+  if (typeof x !== "number" || typeof y !== "number" || typeof z !== "number") {
+    return "please provide a number value";
+  } else if (x < 0 || y < 0 || z < 0) {
+    return "please provide a positive number value";
+  } else {
+    let total = friendOnetotalGem + friendTwototalGem + friendThreetotalGem;
+    let twoTimes = 1000 * 2;
+    if (total > twoTimes) {
+      return total - twoTimes;
+    } else {
+      return total;
+    }
+  }
+}
+console.log(gemsToDiamond(100, 5, 1));
+console.log(gemsToDiamond(20, 200, 50));
+console.log(gemsToDiamond(1, 1, 1));
