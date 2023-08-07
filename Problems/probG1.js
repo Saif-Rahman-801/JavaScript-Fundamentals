decc0835e4267af81e26b0775d7916c67b022f6c
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-07 13:39:04 +0600

    checking file extension

--- Problems/probG1.js
+++ Problems/probG1.js
@@ -14,6 +14,32 @@ function isInteger(num1) {
   }
 }
 
-console.log(isInteger(2.5));
+/* console.log(isInteger(2.5));
 console.log(isInteger(2));
-console.log(isInteger("hello"));
+console.log(isInteger("hello")); */
+
+function isJavaScriptFile(fileName) {
+  if (typeof fileName !== "string") {
+    return "please provide string value";
+  } else {
+    /* if (fileName.endsWith(".js")) {
+      return true;
+    } else {
+      return false;
+    } */
+    return fileName.endsWith(".js");
+  }
+}
+
+function isJavaScriptFile2(fileName) {
+  if (typeof fileName !== "string") {
+    return "please provide string value";
+  } else {
+    const arr = fileName.split(".");// ['index', 'js'......]
+    const lastEl = arr.pop();
+    return lastEl.toLowerCase() === "js";
+  }
+}
+
+// console.log(isJavaScriptFile("index.js"));
+console.log(isJavaScriptFile2("index.js.i"));
