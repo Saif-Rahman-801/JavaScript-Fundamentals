f4b941be07f4dce0f743b40df145c2fab6a443b7
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-07 14:04:38 +0600

    taking positive number

--- Problems/probG1.js
+++ Problems/probG1.js
@@ -35,11 +35,29 @@ function isJavaScriptFile2(fileName) {
   if (typeof fileName !== "string") {
     return "please provide string value";
   } else {
-    const arr = fileName.split(".");// ['index', 'js'......]
+    const arr = fileName.split("."); // ['index', 'js'......]
     const lastEl = arr.pop();
     return lastEl.toLowerCase() === "js";
   }
 }
 
 // console.log(isJavaScriptFile("index.js"));
-console.log(isJavaScriptFile2("index.js.i"));
+// console.log(isJavaScriptFile2("index.js.i"));
+
+function mindGame(pNum) {
+  /* if (typeof pNum !== ("number" || pNum <= 0)) {
+    return "please provide positive number value";
+  } else {
+    const calculationOne = (pNum * 3 + 10) / 2 - 5;
+    return calculationOne;
+  } */
+  if (typeof pNum !== "number") {
+    return "please provide a number";
+  } else if (pNum <= 0) {
+    return "please provide a positive number";
+  } else {
+    const calculationOne = (pNum * 3 + 10) / 2 - 5;
+    return calculationOne;
+  }
+}
+console.log(mindGame(5));
