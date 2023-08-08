8f2435db9cbdcc5236b4433facc2487e7a612a01
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-08 15:06:50 +0600

    object test

--- Problems/probG2.js
+++ Problems/probG2.js
@@ -20,17 +20,17 @@ let arr = [1, 2, 3, -3, -2, -1, -9];
 // console.log(findingBadData(arr));
 
 function gemsToDiamond(x, y, z) {
-  let gemsOneValue = 21;
-  let gemsTwoValue = 32;
-  let gemsThreeValue = 43;
-  let friendOnetotalGem = gemsOneValue * x;
-  let friendTwototalGem = gemsTwoValue * y;
-  let friendThreetotalGem = gemsThreeValue * z;
   if (typeof x !== "number" || typeof y !== "number" || typeof z !== "number") {
     return "please provide a number value";
   } else if (x < 0 || y < 0 || z < 0) {
     return "please provide a positive number value";
   } else {
+    const gemsOneValue = 21;
+    const gemsTwoValue = 32;
+    const gemsThreeValue = 43;
+    const friendOnetotalGem = gemsOneValue * x;
+    const friendTwototalGem = gemsTwoValue * y;
+    const friendThreetotalGem = gemsThreeValue * z;
     let total = friendOnetotalGem + friendTwototalGem + friendThreetotalGem;
     let twoTimes = 1000 * 2;
     if (total > twoTimes) {
@@ -40,6 +40,22 @@ function gemsToDiamond(x, y, z) {
     }
   }
 }
-console.log(gemsToDiamond(100, 5, 1));
-console.log(gemsToDiamond(20, 200, 50));
-console.log(gemsToDiamond(1, 1, 1));
+// console.log(gemsToDiamond(100, 5, 1));
+// console.log(gemsToDiamond(20, 200, 50));
+// console.log(gemsToDiamond(1, 1, 1));
+
+function printDetails(obj) {
+  if (typeof obj !== "object") {
+    return "please provide a object";
+  } else {
+    const name = obj.name || "nai";
+    const age = obj.age || "nai";
+    const mail = obj.mail || "nai";
+    return "amar nam " + name + " amar boyosh " + age + " amar mail " + mail;
+  }
+}
+const obj = {
+  name: "Saif Rahman",
+  age: 20,
+};
+console.log(printDetails(obj));
