77fb720e26c3b39efce0f8bc9249d920fe1d9ac5
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-23 18:16:59 +0600

    spread operator

--- ES6/distructure.js
+++ ES6/distructure.js
@@ -1,7 +1,23 @@
 const actor = {
-    name: "Tom Cruise",
-    age: 60,
-    industry: "hollywood"
-}
-const {name, age, industry} = actor;
+  name: "Tom Cruise",
+  age: 60,
+  industry: "hollywood",
+};
+const obj = Object.keys(actor)
+console.log(obj);
+const { name, age, industry } = actor;
 console.log(age, industry);
+
+const arr = [2, 3, 4];
+const [first, second] = arr;
+console.log(first, second);
+
+const nayikas=['mousumi' , 'popy' ,'shabnur', 'purnima' ]
+const nayikas2 = [...nayikas]
+
+const obj2 = { foo: 1 };
+obj2.bar = 2;
+console.log(obj2);
+
+
+
